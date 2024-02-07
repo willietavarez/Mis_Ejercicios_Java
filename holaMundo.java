ee28ea23d64b2a61d4f884396c1becc7aa67ee65
willietavarez <121048373+willietavarez@users.noreply.github.com>
2024-02-07 11:26:26 -0400

    Create holaMundo.java
    Cracion de primer hola mundo en java

---
+++ holaMundo.java
@@ -0,0 +1,8 @@
+// HolaMundo.java
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        // Imprimir "Hola Mundo" en la consola
+        System.out.println("Hola Mundo");
+    }
+}
