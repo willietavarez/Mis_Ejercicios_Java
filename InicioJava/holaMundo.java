9eb170a55b302c45b26c58e41b876bb01975dec8
willietavarez <121048373+willietavarez@users.noreply.github.com>
2024-02-07 11:47:13 -0400

    nueva actualizacion

--- InicioJava/holaMundo.java
+++ InicioJava/holaMundo.java
@@ -4,6 +4,6 @@ public class HolaMundo {
     public static void main(String[] args) {
         // Imprimir "Hola Mundo" en la consola
         System.out.println("Hola Mundo");
-        
+        //Actualizacion
     }
 }
